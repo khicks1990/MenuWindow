004ed43a17a1f8a49ac27656ae883abb373aa5ff
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 07:08:49 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+
+       /**
+          The main method creates an instance of the
+          MenuWindow class, which causes it to display
+          its window.
+       */
+
+       public static void main(String[] args)
+       {
+          new MenuWindow();
+       }
+}
